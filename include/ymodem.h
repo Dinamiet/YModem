c4291f8d5b9ecdce1e780a1f278ee843d7b03a29
Dinamiet <iceman.sm39@gmail.com>
2021-06-05 16:14:53 +0200

    Rough receive implementation

--- include/ymodem.h
+++ include/ymodem.h
@@ -3,13 +3,14 @@
 
 #include <stdint.h>
 
-typedef uint8_t (*InterfaceRead)(uint8_t *buff, uint32_t maxLen);
-typedef uint8_t (*InterfaceWrite)(uint8_t *buff, uint32_t len);
-typedef uint8_t (*FileRead)(char* fileName, uint8_t *buff, uint32_t maxLen);
-typedef uint8_t (*FileWrite)(char* fileName, uint8_t *buff, uint32_t len);
-typedef void (*Sleep)(uint16_t timeout);
+typedef uint8_t (*InterfaceRead)(uint8_t* buff, uint32_t maxLen);
+typedef uint8_t (*InterfaceWrite)(uint8_t* buff, uint32_t len);
+typedef uint8_t (*FileRead)(char* fileName, uint8_t* buff, uint32_t maxLen);
+typedef uint8_t (*FileWrite)(char* fileName, uint8_t* buff, uint32_t len);
+typedef uint32_t (*Timestamp)();
 
-typedef enum {
+typedef enum
+{
 	OK,
 	TIMEOUT,
 	CANCLED,
@@ -18,12 +19,12 @@ typedef enum {
 
 typedef struct
 {
-	InterfaceRead Read; // Read data from interface for protocol
+	InterfaceRead  Read;  // Read data from interface for protocol
 	InterfaceWrite Write; // Write data to interface for protocol
-	Sleep		   Wait;
+	Timestamp	   Time;
 } YModem;
 
-void YModem_Init(YModem* modem, InterfaceRead readFunc, InterfaceWrite writeFunc, Sleep waitFunc);
+void		 YModem_Init(YModem* modem, InterfaceRead readFunc, InterfaceWrite writeFunc, Timestamp timeFunc);
 YModemReturn YModem_Receive(YModem* modem, FileWrite writeFunc);
 YModemReturn YModem_Transmit(YModem* modem, FileRead readFunc, uint32_t size);
 
--- src/ymodem.c
+++ src/ymodem.c
@@ -1,7 +1,7 @@
 #include "ymodem.h"
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define STX 0x02 // 1024 byte data block
 #define SOH 0x01 // 128 byte data block
@@ -11,12 +11,16 @@
 #define CAN 0x18 // Cancel transmission
 #define C	0x43 // Ready to receive data
 
-#define RETRIES 10 // number of attempts
-#define TIMEOUT 1000  // ms
+#define RETRIES 10	 // number of attempts
+#define TIMEOUT 1000 // ms
 
 #define DATA_SIZE		   1024
+#define FILE_NAME		   32
 #define LARGE_PACKET_BYTES 1024
 #define SMALL_PACKET_BYTES 128
+
+#define HEADER_SIZE sizeof(PacketHeader)
+#define CRC_SIZE	2
 
 typedef struct
 {
@@ -27,91 +31,133 @@ typedef struct
 
 typedef enum
 {
+	FILENAME,
+	DATA,
+	DONE
+} TransferState;
+
+typedef enum
+{
 	INIT,
-	WAIT_FIRST_HEADER,
 	WAIT_HEADER,
 	WAIT_DATA,
 	WAIT_CRC,
 	SEND_NACK,
 	SEND_ACK
-
 } ReceiveStates;
 
 static bool receiveTimeout(YModem* modem, uint8_t* buff, uint32_t recvLen)
-{	
-	uint32_t recved= 0;
-	uint32_t start= modem->Wait();
-	uint32_t timeDelta= 0;
+{
+	uint32_t recved	   = 0;
+	uint32_t start	   = modem->Time();
+	uint32_t timeDelta = 0;
 	while (recved != recvLen && timeDelta < TIMEOUT)
-		recved= modem->Read(buff, recvLen-recved);
-	
+	{
+		recved	  = modem->Read(buff, recvLen - recved);
+		timeDelta = modem->Time() - start;
+	}
+
 	return recved == recvLen;
 }
 
-void YModem_Init(YModem* modem, InterfaceRead readFunc, InterfaceWrite writeFunc, Sleep waitFunc)
+void YModem_Init(YModem* modem, InterfaceRead readFunc, InterfaceWrite writeFunc, Timestamp timeFunc)
 {
 	modem->Read	 = readFunc;
 	modem->Write = writeFunc;
-	modem->Wait	 = waitFunc;
+	modem->Time	 = timeFunc;
 }
 
 YModemReturn YModem_Receive(YModem* modem, FileWrite writeFunc)
 {
-	ReceiveStates state= INIT;
-	YModemReturn returnValue= OK;
+	ReceiveStates state		  = INIT;
+	YModemReturn  returnValue = OK;
 
-	uint8_t		 retriesRemaining;
-	uint8_t		 controlByte;
-	uint16_t 	 buffOffset;
-	uint32_t	 blockCounter;
-	PacketHeader* header;
-	uint16_t* CRC;
-	uint8_t Data[DATA_SIZE+sizeof(PacketHeader)+sizeof(uint16_t)];
-	header= Data;
+	TransferState transfer;
+	char*		  fileName[FILE_NAME];
+	uint32_t	  fileSize = 0;
+	uint8_t		  retriesRemaining;
+	uint8_t		  controlByte;
+	uint32_t	  blockCounter;
+	uint16_t	  dataLen = 0;
+	uint8_t		  buffer[DATA_SIZE + sizeof(PacketHeader) + sizeof(uint16_t)];
+	PacketHeader* header = buffer;
+	uint8_t*	  data	 = &buffer[HEADER_SIZE];
+	uint16_t*	  crc;
 
+	retriesRemaining = RETRIES;
 	while (true)
 	{
 		switch (state)
 		{
 			case INIT:
-				// Initialize all state variables
-				// Send C
-				// Move to waiting for first header
+				controlByte	 = C;
+				blockCounter = 0;
+				transfer = FILENAME;
+				modem->Write(&controlByte, 1);
+				state = WAIT_HEADER;
 				break;
-			case WAIT_FIRST_HEADER:
-				// sub 1 from retries -> if none left return with timeout
-				// Receive header or wait TIMEOUT
-				// if timeout return to INIT
-				// else goto WAIT_DATA
-				break;
+
 			case WAIT_HEADER:
-				// sub 1 from retries -> if none left return with timeout
-				// Receive header or wait TIMEOUT
-				// if timeout goto NACK
-				// else goto WAIT_DATA
+				header->Type = C;
+				if (receiveTimeout(modem, header, HEADER_SIZE))
+				{
+					dataLen = header->Type == STX ? LARGE_PACKET_BYTES : SMALL_PACKET_BYTES;
+					state	= WAIT_DATA;
+				}
+				else if (header->Type == EOT) // transfer done
+					state = INIT;			  // start next transfer
+				else if (header->Type == CAN) // transfer cancled
+					return CANCLED;
+				else if (retriesRemaining-- == 0) // receive timeout - there is no more retries left
+					return TIMEOUT;
+				else												 // receive timeout - retry
+					state = transfer == FILENAME ? INIT : SEND_NACK; // if filename is was not yet received - continue to init else send NACK
+
 				break;
+
 			case WAIT_DATA:
-				// receive data (size depending on header) or wait TIMEOUT
-				// if timeout goto NACK
-				// else goto WAIT_CRC
+				if (receiveTimeout(modem, data, dataLen))
+				{
+					crc	  = &data[dataLen];
+					state = WAIT_CRC;
+				}
+				else
+					state = SEND_NACK;
 				break;
+
 			case WAIT_CRC:
-				// receive CRC or wait TIMEOUT
-				// if timeout goto NACK
-				// else check block numers
-				// 		check CRC
-				//		if CRC match -> goto ACK else goto NACK
+				if (receiveTimeout(modem, crc, CRC_SIZE))
+				{
+					// TODO: Check CRC and block counters - if fail send NACK
+					state = SEND_ACK;
+				}
+				else
+					state = SEND_NACK;
+
 				break;
+
 			case SEND_NACK:
-				// SEND NACK
-				// goto WAIT_HEADER
+				controlByte = NAK;
+				modem->Write(&controlByte, 1);
+				state = WAIT_HEADER;
 				break;
+
 			case SEND_ACK:
-				// SEND ACK
-				// RESET retries
-				// goto WAIT_HEADER
-			
+				if (transfer == FILENAME)
+				{
+					// TODO: capture filename and size
+					transfer= DATA;
+				}
+				else if (transfer == DATA)
+				{
+					// TODO: Write data to file
+				}
+				controlByte = ACK;
+				modem->Write(&controlByte, 1);
+				retriesRemaining = RETRIES;
+				state			 = WAIT_HEADER;
 				break;
+
 			default:
 				return UNKNOWN_STATE;
 				break;
