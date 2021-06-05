6f30d053612e1a43a86f74fe1ee1c889643345d9
Dinamiet <iceman.sm39@gmail.com>
2021-06-05 10:30:50 +0200

    Function and structure definitions

--- include/ymodem.h
+++ include/ymodem.h
@@ -1,6 +1,27 @@
 #ifndef __YMODEM_H__
 #define __YMODEM_H__
 
-void YModem_Init();
+#include <stdint.h>
+
+typedef uint8_t (*InterfaceRead)(uint8_t *buff, uint32_t maxLen);
+typedef uint8_t (*InterfaceWrite)(uint8_t *buff, uint32_t len);
+typedef uint8_t (*FileRead)(char* fileName, uint8_t *buff, uint32_t maxLen);
+typedef uint8_t (*FileWrite)(char* fileName, uint8_t *buff, uint32_t len);
+
+typedef enum {
+	OK,
+	TIMEOUT,
+	CANCLED
+} YModemReturn;
+
+typedef struct
+{
+	InterfaceRead Read; // Read data from interface for protocol
+	InterfaceWrite Write; // Write data to interface for protocol
+} YModem;
+
+void YModem_Init(YModem* modem, InterfaceRead readFunc, InterfaceWrite writeFunc);
+YModemReturn YModem_Receive(YModem* modem, FileWrite writeFunc);
+YModemReturn YModem_Transmit(YModem* modem, FileRead readFunc);
 
 #endif
--- src/ymodem.c
+++ src/ymodem.c
@@ -12,4 +12,33 @@
 #define SHORT_TIMEOUT 1	 // 1 seconds
 #define LONG_TIMEOUT  10 // 10 seconds
 
-void YModem_Init() { return; }
+#define BIG_PACKET_DATA_SIZE   1024
+#define SMALL_PACKET_DATA_SIZE 128
+
+typedef struct
+{
+	uint8_t	 Type;
+	uint8_t	 BlockNumber;
+	uint8_t	 BlockNumberComplement;
+	uint8_t	 Data[BIG_PACKET_DATA_SIZE];
+	uint16_t CRC;
+} BigPacket;
+
+typedef struct
+{
+	uint8_t	 Type;
+	uint8_t	 BlockNumber;
+	uint8_t	 BlockNumberComplement;
+	uint8_t	 Data[SMALL_PACKET_DATA_SIZE];
+	uint16_t CRC;
+} SmallPacket;
+
+void YModem_Init(YModem* modem, InterfaceRead readFunc, InterfaceWrite writeFunc);
+{
+	modem->Read	 = readFunc;
+	modem->Write = writeFunc;
+}
+
+YModemReturn YModem_Receive(YModem* modem, FileWrite writeFunc) { return OK; }
+
+YModemReturn YModem_Transmit(YModem* modem, FileRead readFunc) { return OK; }
