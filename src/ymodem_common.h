2860adf4695a42510234e7a3370fa8efb183f652
Dinamiet <iceman.sm39@gmail.com>
2021-06-16 10:32:15 +0200

    Split receive and transmit

---
+++ src/ymodem_common.h
@@ -0,0 +1,26 @@
+#include "ymodem.h"
+
+#include "crc16.h"
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <endian.h>
+
+#define STX 0x02 // 1024 byte data block
+#define SOH 0x01 // 128 byte data block
+#define EOT 0x04 // End of transfer
+#define ACK 0x06 // Acknownledged, continue transfer
+#define NAK 0x15 // Not Acknownledged, retry transfer
+#define CAN 0x18 // Cancel transmission
+#define C	0x43 // Ready to receive data
+
+#define MAX_RETRIES 10	 // number of attempts
+#define TIMEOUT_MS	1000 // ms
+
+#define DATA_SIZE		   1024
+#define FILE_NAME		   32
+#define LARGE_PACKET_BYTES 1024
+#define SMALL_PACKET_BYTES 128
+
+#define MIN(x, y) (x > y ? y : x)
---
+++ src/ymodem_transmit.c
@@ -0,0 +1,9 @@
+#include "ymodem_common.h"
+
+YModemReturn YModem_Transmit(YModem* modem, FileRead readFunc, uint32_t size)
+{
+	(void)modem;
+	(void)readFunc;
+	(void)size;
+	return SUCC;
+}
