d717e4138bde027078d7c0c5987a8c6461060d01
Dinamiet <iceman.sm39@gmail.com>
2021-06-16 11:50:12 +0200

    Untested transmit implementation

--- include/ymodem.h
+++ include/ymodem.h
@@ -5,7 +5,7 @@
 
 typedef uint32_t (*InterfaceRead)(uint8_t* buff, uint32_t maxLen);
 typedef uint32_t (*InterfaceWrite)(uint8_t* buff, uint32_t len);
-typedef uint32_t (*FileRead)(char* fileName, uint8_t* buff, uint32_t maxLen);
+typedef uint32_t (*FileRead)(char* fileName, uint8_t* buff, uint32_t offset, uint32_t maxLen);
 typedef uint32_t (*FileWrite)(char* fileName, uint8_t* buff, uint32_t len);
 typedef uint32_t (*Timestamp)();
 
@@ -26,6 +26,6 @@ typedef struct
 
 void		 YModem_Init(YModem* modem, InterfaceRead readFunc, InterfaceWrite writeFunc, Timestamp timeFunc);
 YModemReturn YModem_Receive(YModem* modem, FileWrite writeFunc);
-YModemReturn YModem_Transmit(YModem* modem, FileRead readFunc, uint32_t size);
+YModemReturn YModem_Transmit(YModem* modem, char* fileNames[], uint32_t sizes[], uint8_t numFiles, FileRead readFunc);
 
 #endif
